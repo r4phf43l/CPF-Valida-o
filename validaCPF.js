7860d8a3556d7893e1463f7fbdd05205359447c9
r4phf43l <61799987+r4phf43l@users.noreply.github.com>
2020-06-29 14:09:43 -0300

    Update validaCPF.js

--- validaCPF.js
+++ validaCPF.js
@@ -1,10 +1,10 @@
-    function validaCPF(cpf) {
-        cpf = cpf.toString().replace(/[^\d]+/g,'');	
-        if (cpf.length != 11) { return false; }
-        if (cpf.match('/(\d)\1{10}/')) { return false; }
-        for (t = 9; t < 11; t++) {
-            for (d = 0, c = 0; c < t; c++) { d += cpf[c] * ((t + 1) - c); }
-            d = ((10 * d) % 11) % 10;
-            if (cpf[c] != d) { return false; }
-        } return true;
-    }
+function validateCPF(cpf) {
+    cpf = cpf.toString().replace(/[^\d]+/g,'');	
+    if (cpf.length != 11) { return false; }
+    if (cpf.match('/(\d)\1{10}/')) { return false; }
+    for (t = 9; t < 11; t++) {
+        for (d = 0, c = 0; c < t; c++) { d += cpf[c] * ((t + 1) - c); }
+        d = ((10 * d) % 11) % 10;
+        if (cpf[c] != d) { return false; }
+    } return true;
+}
